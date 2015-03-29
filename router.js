7015dd0331475fbf00d6327b169158d9ed17471a
Cristina Rojas <telematicacristina@gmail.com>
2015-03-28 17:11:51 -0700

    Adding router.js

---
+++ router.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var config = require('./config/config');
+var routes = require('./controllers/index');
+var users = require('./controllers/users');
+
+
+module.exports = function(app) {
+  app.use('/', routes); // ruta barra nav, archivo controlador
+  app.use('/users', users);
+};
