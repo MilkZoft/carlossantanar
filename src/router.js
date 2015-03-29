138ee6dd2e8b80616fdda4767afa0a95fce57d31
Carlos Santana <carlos.x.santana.-nd@disney.com>
2015-03-29 02:24:23 -0700

    Adding defaultController to router, also css and js files

--- src/router.js
+++ src/router.js
@@ -1,11 +1,44 @@
 'use strict';
 
-var config = require('./config/config');
-var home = require('./controllers/home');
-var users = require('./controllers/users');
+var config = require('./lib/config');
+var availableLanguages = config().languages.list.join('|');
+var defaultController;
+var homeController;
 
 module.exports = function(app) {
-  // Path from browser
-  app.use('/', home);
-  app.use('/users', users);
+  defaultController = require('./controllers/' + config().controllers.default);
+  homeController = require('./controllers/home');
+
+  // Load necessary helpers
+  var i18n = require('./lib/helpers/i18n');
+  var utils = require('./lib/helpers/utils');
+
+  // Loading isMobile, basePath, currentLanguage and __
+  app.use(function(req, res, next) {
+    res.locals.isMobile = utils.isMobile(req.headers['user-agent']);
+    res.locals.basePath = config().baseUrl + '/' + i18n.getCurrentLanguage(req.url);
+    res.locals.currentLanguage = i18n.getCurrentLanguage(req.url);
+    res.locals.__ = i18n.load(i18n.getCurrentLanguage(req.url));
+    next();
+  });
+
+  // default css and js
+  app.use(function(req, res, next) {
+    res.locals.css = [
+      '/css/style.css'
+    ];
+
+    res.locals.bottomJs = [
+      '/bower_components/angular/angular.js',
+      '/bower_components/lodash/dist/lodash.min.js',
+      '/bower_components/restangular/dist/restangular.js'
+    ];
+
+    next();
+  });
+
+  // Controllers dispatch
+  app.use('/', defaultController);
+  app.use('/:language(' + availableLanguages + ')', defaultController);
+  app.use('/:language(' + availableLanguages + ')/home', homeController);
 };
