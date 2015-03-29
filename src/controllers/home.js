e28e904ef53c031993e214452160a96bd84fdc68
Cristina Rojas <telematicacristina@gmail.com>
2015-03-28 21:14:49 -0700

    Reneaming index controller to home controller

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { name: 'Carlos Santana1' });
+  res.render('home/welcome', { name: 'Carlos Santana' });
 });
 
 module.exports = router;
